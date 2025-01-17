037d142a11b752da398ee9e8dd4b16f00bad7b87
Lord <110597375+kd2929@users.noreply.github.com>
2025-01-17 23:20:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from PIL import Image, UnidentifiedImageError
 from fpdf import FPDF
 
 # Bot configurations
-API_ID = API_ID  # Your API_ID
-API_HASH = "HASH"  # Your API_HASH
-BOT_TOKEN = "TOKEN"  # Your BOT_TOKEN
+API_ID = "24250238"  # Your API_ID
+API_HASH = "cb3f118ce5553dc140127647edcf3720"  # Your API_HASH
+BOT_TOKEN = "6687465225:AAHF-yb3LNKeFZzJUQEZIj9zgpp6k-o56qo"  # Your BOT_TOKEN
 
 # Initialize the bot
 app = Client("book_downloader_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
